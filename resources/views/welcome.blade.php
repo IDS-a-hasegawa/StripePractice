6697b0690d234f87e74fff9b0e1fcb005423f0d2
tech-camp <tech-camp@tech-camp-1-109-no-MacBook-Air.local>
2017-05-26 16:15:40 +0900

    commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Laravel 5</div>
+
+                <form action="/" method="POST">
+                  <script
+                    src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+                    data-key="pk_test_xBxVZlfllqUc8kMo3Vz31ZGs"
+                    data-amount="100"
+                    data-name="TEST"
+                    data-description="TESTTEST"
+                    data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
+                    data-currency="jpy">
+                  </script>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
